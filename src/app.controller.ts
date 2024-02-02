985274fa5b2708e5849394057c654b9df8ea1f35
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-02 18:17:28 +0100

    Interceptor qui filtre ou remplace une donnée entrante

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,13 +3,14 @@ import { AppService } from './app.service';
 import { MesureDurationInterceptor } from './mesure-duration/mesure-duration.interceptor';
 import { LogClientsInterceptor } from './log-clients/log-clients.interceptor';
 import { EnrichResponseInterceptor } from './enrich-response/enrich-response.interceptor';
+import { FilterRequestInterceptor } from './filter-request/filter-request.interceptor';
 
 @Controller('api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('hello/:name')
-  @UseInterceptors(MesureDurationInterceptor, LogClientsInterceptor, EnrichResponseInterceptor)
+  @UseInterceptors(MesureDurationInterceptor, FilterRequestInterceptor)
   getHello(@Param('name') name: string): string {
     return this.appService.getHello(name);
   }
