0798ea1db686a7566018fd1cc12c90514b8c5d09
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-02 17:35:30 +0100

    Implémenter la mesure de la durée d'exécution de la requête et de la réponse

--- src/mesure-duration/mesure-duration.interceptor.ts
+++ src/mesure-duration/mesure-duration.interceptor.ts
@@ -4,10 +4,11 @@ import { Observable, tap } from 'rxjs';
 @Injectable()
 export class MesureDurationInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('intercepting request', context);
+    const startDate = Date.now();
+    //console.log('intercepting request', context);
     return next.handle().pipe(
       tap(valueFromRouteHandler => 
-        console.log('After controller sending response', valueFromRouteHandler.toUpperCase()))
+        console.log(`Duration in ms: ${Date.now() - startDate}`))
     );
   }
 }
